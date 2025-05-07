8ec53c1c498e3a86cdc8026fde7be93ef6cf224f
taityunne <taityun0903@gmail.com>
2025-05-07 20:19:40 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+   
+let hdig = document.getElementById('headerimg');
+
+hdig.addEventListener('dblclick', function() {
+    document.querySelector('#arrange iframe').classList.toggle('wactive');
+    document.querySelector('#base iframe').classList.toggle('wactive');
+    
+
+},false)
+
+let bg = document.getElementsByClassName('iframe')[0];
+bg.addEventListener('click', function() {
+    document.querySelector('.iframe').classList.toggle('iactive');
+    document.querySelector('header').classList.toggle('iactive');
+    document.querySelector('#hd header img').src = 'img/h_k_bad_apple_copy.png'
+    document.querySelector('#hd .iactive img').src = 'img/k_bad_apple_copy.png'
+},false)
+})
